7a7f159b4b6128720ca334033094759778f17eaa
krutikaparab9392 <krutikaparab9392@gmail.com>
2019-03-01 14:33:16 -0600

    Delete The PADS.sql

--- The PADS.sql
+++
@@ -1,7 +0,0 @@
-SELECT name||'('||SUBSTR(occupation,1,1)||')'
-FROM occupations
-ORDER BY name;
-SELECT 'There are total '||COUNT(occupation)||' '||LOWER(occupation)||'s.'
-FROM occupations
-GROUP BY occupation
-ORDER BY COUNT(occupation),occupation;
